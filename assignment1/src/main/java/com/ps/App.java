5934729d04a7adb091eb286dc4ab6935af2b14f6
P-S-5 <thepiyushsahoo@gmail.com>
2024-11-16 17:01:11 +0530

    updatd java jdk 17->21

--- assignment1/src/main/java/com/ps/App.java
+++ assignment1/src/main/java/com/ps/App.java
@@ -1,10 +1,8 @@
 package com.ps;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
         System.out.println("This is assignment1");
     }
 }
